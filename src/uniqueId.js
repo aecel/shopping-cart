f2887c8e55b068d94f1fe670c31e974e1c0b2c0c
aecel <aecelacuna@gmail.com>
2022-10-10 19:50:45 +0800

    First testing

---
+++ src/uniqueId.js
@@ -0,0 +1,10 @@
+const uniqueId = () => {
+  return (
+    Date.now().toString(36) +
+    Math.floor(
+      Math.pow(10, 12) + Math.random() * 9 * Math.pow(10, 12)
+    ).toString(36)
+  )
+}
+
+export default uniqueId
